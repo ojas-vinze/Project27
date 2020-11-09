9b0922e950ce52a6858a95eda861ebf449f9f3e7
ojas-vinze <71540421+ojas-vinze@users.noreply.github.com>
2020-11-09 22:32:32 +0530

    Update Rope.js

--- Rope.js
+++ Rope.js
@@ -1,46 +1,20 @@
+class Rope{ 
+    constructor(vbodyA,vbodyB,voffsetX,voffsetY){ 
+        this.offsetX=voffsetX;
+        this.offsetY=voffsetY;
+        var options={
+            bodyA:vbodyA,
+            bodyB:vbodyB,
+            pointB:{x:this.offsetX,y:this.offsetY}
+        }
+        this.chain=Constraint.create(options);
+        World.add(world,this.chain);
+    }
 
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Body = Matter.Body;
-
-function preload()
-{
-	
-}
-
-function setup() {
-	createCanvas(800, 700);
-
-
-	engine = Engine.create();
-	world = engine.world;
-
-	//Create the Bodies Here.
-	roof = new Roof(width/2,height+10,width,20);
-
-	bob1 = new bob(width/2,height-20,20);
-	// bob2 = new bob(width/2-10)
-
-	rope1 = new Rope(bob1.body,roof.body,-100,0);
-	rope2 = new Rope(bob2.body,roof.body,-100,0);
-	rope3 = new Rope(bob3.body,roof.body,-100,0);
-	rope4 = new Rope(bob4.body,roof.body,-100,0);
-
-	Engine.run(engine);
-  
-}
-
-
-function draw() {
-  rectMode(CENTER);
-  background(0);
-  
-  rope1.display();
-  rope2.display();
-  rope3.display();
-  rope4.display();
-}
-
-
-
+    display(){
+        var posA=this.chain.bodyA.position;
+        var posB=this.chain.bodyB.position;
+        strokeWeight(2);
+        line(posA.x,posA.y,posB.x+this.offsetX,posB.y+this.offsetY); 
+        } 
+    }
